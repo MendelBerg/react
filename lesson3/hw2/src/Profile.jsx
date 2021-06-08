90c22b1135989217cfb2a51ff3a82e3cf80a9179
Alex <mendelberg44@gmail.com>
2021-06-08 23:32:04 +0300

    l3 h2 v5

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -1,35 +1,23 @@
 import React from 'react';
 
-// const getDate = (date) => date.getDate();
-// const getMonth = (date) =>
-//   date.toLocaleString('en', {
-//     month: 'short',
-//   });
-// const getYear = (date) => date.getFullYear().toString().substr(-2);
-
-// const getTimeItem = (time, date) => {
-//   if (time === 'date') return new Date(date).getDate();
-//   if (time === 'month')
-//     return new Date(date).toLocaleString('en', {
-//       month: 'short',
-//     });
-//   return new Date(date).getFullYear().toString().substr(-2);
-// };
-
 export default (props) => {
   const user = props.userData;
+  const date = new Date(user.birthDate).getDate();
+  const month = new Date(user.birthDate).toLocaleString('en', {
+    month: 'short',
+  });
+  const year = +new Date(user.birthDate).getFullYear().toString().substr(-2);
+  console.log(date);
+  console.log(month);
+  console.log(year);
+
   return (
     <div className="profile">
       <div className="profile__name">
         {user.firstName} {user.lastName}
       </div>
       <div className="profile__birth">
-        {`Was born ${new Date(user.birthDate).getDate()} 
-        ${new Date(user.birthDate).toLocaleString('en', {
-          month: 'short',
-        })} 
-        ${new Date(user.birthDate).getFullYear().toString().substr(-2)} 
-        in ${user.birthPlace}`}
+        {`Was born ${date} ${month} ${year} in ${user.birthPlace}`}
       </div>
     </div>
   );
