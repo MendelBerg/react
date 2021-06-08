16b853698f33f2b369dc3e44f59ab8e4f35d6185
Alex <mendelberg44@gmail.com>
2021-06-08 23:21:10 +0300

    l3 h2 v3

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const getDate = (date) => date.getDate();
-const getMonth = (date) =>
-  date.toLocaleString('en', {
-    month: 'short',
-  });
-const getYear = (date) => date.getFullYear().toString().substr(-2);
+// const getDate = (date) => date.getDate();
+// const getMonth = (date) =>
+//   date.toLocaleString('en', {
+//     month: 'short',
+//   });
+// const getYear = (date) => date.getFullYear().toString().substr(-2);
 
 const getTimeItem = (time, date) => {
-  if (time === 'date') return getDate(new Date(date));
-  if (time === 'month') return getMonth(new Date(date));
-  return getYear(new Date(date));
+  if (time === 'date') return new Date(date).getDate();
+  if (time === 'month')
+    return new Date(date).toLocaleString('en', {
+      month: 'short',
+    });
+  return new Date(date).getFullYear().toString().substr(-2);
 };
 
 export default (props) => {
