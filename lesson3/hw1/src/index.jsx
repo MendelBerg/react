bb890ed8feae1461ac7f19c2c34867df799a78d4
Alex <mendelberg44@gmail.com>
2021-06-08 18:53:47 +0300

    l3 h1 v2

--- lesson3/hw1/src/index.jsx
+++ lesson3/hw1/src/index.jsx
@@ -5,14 +5,11 @@ import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-const getAge = (date) => {
-  const ageDate = new Date(new Date() - date.getTime());
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
-};
-
-const date = new Date('2001-06-09T11:11:11.819Z');
-
 ReactDOM.render(
-  <Greeting firstName="John" lastName="Doe" birthDate={getAge(date)} />,
+  <Greeting
+    firstName="John"
+    lastName="Doe"
+    birthDate={new Date('2001-06-09T11:11:11.819Z')}
+  />,
   rootElement
 );
