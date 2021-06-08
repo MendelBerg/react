3e6a11ead3bf7e681819ca1ec7479480bc8dbd01
Alex <mendelberg44@gmail.com>
2021-06-08 23:38:14 +0300

    l3 h2 v8

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default (props) => {
   const user = props.userData;
-  const date = new Date(user.birthDate).getDate() + '';
-  const month = new Date(user.birthDate).toLocaleString('en', {
-    month: 'short',
-  });
-  const year = new Date(user.birthDate).getFullYear().toString().substr(-2);
+  // const date = new Date(user.birthDate).getDate() + '';
+  // const month = new Date(user.birthDate).toLocaleString('en', {
+  //   month: 'short',
+  // });
+  // const year = new Date(user.birthDate).getFullYear().toString().substr(-2);
 
   return (
     <div className="profile">
@@ -14,7 +14,14 @@ export default (props) => {
         {user.firstName} {user.lastName}
       </div>
       <div className="profile__birth">
-        {`Was born ${date} ${month} ${year} in ${user.birthPlace}`}
+        {`Was born ${new Date(user.birthDate).getDate()} ${new Date(
+          user.birthDate
+        ).toLocaleString('en', {
+          month: 'short',
+        })} ${new Date(user.birthDate)
+          .getFullYear()
+          .toString()
+          .substr(-2)} in ${user.birthPlace}`}
       </div>
     </div>
   );
