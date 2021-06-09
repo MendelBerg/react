8fd1ffa8e984259ab941f14dd9c58b844c9b86fe
Alex <mendelberg44@gmail.com>
2021-06-09 12:52:53 +0300

    l3 h2 v14

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -4,7 +4,6 @@ import moment from 'moment';
 export default (props) => {
   const user = props.userData;
   const formatedData = moment(user.birthDate).format('DD MMM YY');
-  
 
   return (
     <div className="profile">
