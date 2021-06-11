ff21d73ddcd18a750bbee0dad24a40318c244680
Alex <mendelberg44@gmail.com>
2021-06-11 20:34:19 +0300

    l7 a1 2

--- lesson7/add1/webpack.config.js
+++ lesson7/add1/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -26,6 +27,11 @@ module.exports = (env, argv) => {
         },
       ],
     },
+
+    output: {
+      path: path.resolve('./', 'review_build')
+    },
+
     plugins: [
       new webpack.ProgressPlugin(),
       new CleanWebpackPlugin(),
