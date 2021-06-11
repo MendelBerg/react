92c6a4d0488a0ad039d6fdaa4ecf98caa0592231
Alex <mendelberg44@gmail.com>
2021-06-11 15:07:14 +0300

    l7 t1

---
+++ lesson7/task1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NumberList from './NumberList.jsx';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+const numbers = [1,2,3,4,5];
+
+ReactDOM.render(<NumberList numbers={numbers} />, rootElement);
---
+++ lesson7/task2/src/Status.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Status = () => {
+  return (
+    <div className="status">
+      Hello
+    </div>
+  );
+};
+
+export default Status;
