d75d4cbf2a7fa7888f1e87efae7598e12628a318
Alex <mendelberg44@gmail.com>
2021-06-12 12:20:41 +0300

    l8 t4

--- lesson8/task4/src/index.jsx
+++ lesson8/task4/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Auth from './Auth.jsx';
+import User from './User.jsx';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Auth />, rootElement);
+ReactDOM.render(<User userId="facebook"/>, rootElement);
