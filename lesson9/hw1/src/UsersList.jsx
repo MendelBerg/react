41760132567c6262d966e0bc028c38073e9dd08c
Alex <mendelberg44@gmail.com>
2021-06-12 20:29:13 +0300

    l9 h1 2

--- lesson9/hw1/src/UsersList.jsx
+++ lesson9/hw1/src/UsersList.jsx
@@ -5,37 +5,35 @@ import Filter from './Filter.jsx';
 class UsersList extends Component {  
   state = {
     value: '',
-    users: this.props.users,
   };
 
   handleChange = (event) => {
     const {value} = event.target;
     this.setState({value});
-    this.filterUsers(value);
   }
 
   filterUsers(value){
-    this.setState({
-      users: value === '' 
-        ? this.props.users 
-        : this.props.users.filter(
-            ({name}) => name.toLowerCase() === value.toLowerCase()
-          )
-    });
+    return value === '' 
+      ? this.props.users 
+      : this.props.users.filter(
+          ({name}) => name.toLowerCase() === value.toLowerCase()
+        )
   }
 
   render(){
+    const users = this.filterUsers(this.state.value);
+
     return (
       <div>
         <div className="filter">
           {<Filter 
             filterText={this.state.value} 
-            count={this.state.users.length} 
+            count={users.length} 
             onChange={this.handleChange}
           />}
         </div>
         <ul className="users">
-          {this.state.users.map(user => <User key={user.id} {...user}/>)}
+          {users.map(user => <User key={user.id} {...user}/>)}
         </ul>
       </div>
     );
