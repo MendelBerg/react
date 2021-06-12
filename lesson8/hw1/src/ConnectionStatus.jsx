fcd67cc65050a7b2b75c0bd08837a04f15fa352e
Alex <mendelberg44@gmail.com>
2021-06-12 13:05:17 +0300

    l8 hw1

---
+++ lesson8/hw1/src/ConnectionStatus.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+class ConnectionStatus extends Component {
+  state = {
+    online: null,
+    offline: null,
+    classEl: null,
+    textEl: null,
+  };
+
+  componentDidMount(){
+    this.showOnline();
+    window.addEventListener('online', this.showOnline);
+    window.addEventListener('offline', this.showOffline);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.showOnline);
+    window.removeEventListener('offline', this.showOffline);
+  }
+
+  showOnline = () => {
+    this.setState({
+      online: true,
+      offline: false,
+      classEl: "status",
+      textEl: "Online",
+    });
+  }
+
+  showOffline = () => {
+    this.setState({
+      online: false,
+      offline: true,
+      classEl: "status status_offline",
+      textEl: "Offline",
+    });
+  }
+
+  render(){
+    return <div className={this.state.classEl}>{this.state.textEl}</div>;    
+  };
+};
+
+export default ConnectionStatus;
--- lesson8/hw1/src/index.jsx
+++ lesson8/hw1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Auth from './Auth.jsx';
+import ConnectionStatus from './ConnectionStatus.jsx';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Auth />, rootElement);
+ReactDOM.render(<ConnectionStatus />, rootElement);
