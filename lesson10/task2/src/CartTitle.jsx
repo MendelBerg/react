a5fc3c1b88a8971f813b25e6800adeb053f9f76d
Alex <mendelberg44@gmail.com>
2021-06-13 19:46:26 +0300

    l10 t2 4

--- lesson10/task2/src/CartTitle.jsx
+++ lesson10/task2/src/CartTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CartTitle = ({userName, count}) => {
   return (
-    <div className="card-title">
+    <div className="cart-title">
       {`${userName}, you added ${count} items`}
     </div>
   );
