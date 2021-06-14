54c067f8f0a9b0666ccc4c23560bd337dd998c3e
Alex <mendelberg44@gmail.com>
2021-06-14 15:59:33 +0300

    l12 init

--- lesson11/add1/src/withDataLoader.jsx
+++ lesson11/add1/src/withDataLoader.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {Component} from "react";
 
-export const withDataLoader = () => {
-  fetchUserData = userId => {
-    const userUrl = `https://api.github.com/users/${userId}`;
-    fetch(userUrl)
-    .then(response => response.json())
-    .then(userData => 
-        this.setState({
-          userData,
-        }),
-    );
-  };
-};
+// !BAD!
+export function withDataLoader(url) {
+  // return (WrapperComponent) => 
+  //   class extends Component {
+  //     constructor(props){
+  //       super(props);
+  //       this.state = {
+  //         data: this.fetchData(url),
+  //         component: WrapperComponent,
+  //       }
+  //     }
+
+  //     fetchData = url => {
+  //       fetch(url)
+  //       .then(response => response.json())
+  //       .then(data => 
+  //           this.setState({
+  //             data,
+  //           }),
+  //       );
+  //     };
+
+  //     render() {
+  //       console.log('state', this.state.data)
+  //       return this.state.component(this.state.data);
+  //     }
+  //   }
+}
