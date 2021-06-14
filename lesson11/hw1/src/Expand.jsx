bcd259fae89d7f8faea2f85dceff8ef606dcb063
Alex <mendelberg44@gmail.com>
2021-06-14 12:56:14 +0300

    l11 h1

--- lesson11/hw1/src/Expand.jsx
+++ lesson11/hw1/src/Expand.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
 const Expand = ({isOpen, title, children, onClick}) => {
-  const btn = !isOpen 
-      ? <i className="fas fa-chevron-down"></i> 
-      : <i className="fas fa-chevron-up"></i>; 
-
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={onClick}>
-          {btn}
-        </button>
+        <button className="expand__toggle-btn" onClick={onClick}>{
+          !isOpen 
+            ? <i className="fas fa-chevron-down"></i> 
+            : <i className="fas fa-chevron-up"></i>
+        }</button>
       </div>
-      <div className="expand__content" style={{display: isOpen ? 'flex' : 'none'}}>{children}</div>
+      {!isOpen ? null : <div className="expand__content">{children}</div>}
     </div>
   );
 };
